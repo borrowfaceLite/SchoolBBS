7a896b935b68852296084438e9820b4da4f48d36
borrowface <borrowface@gmail.com>
2018-06-11 23:51:21 +0800

    borrowface 添加了EditText样式，完善添加评论

--- app/src/main/java/com/hwmlygr/ground/schoolbbs/AddCommentActivity.java
+++ app/src/main/java/com/hwmlygr/ground/schoolbbs/AddCommentActivity.java
@@ -1,13 +1,35 @@
 package com.hwmlygr.ground.schoolbbs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class AddCommentActivity extends Activity {
+import java.io.File;
 
+public class AddCommentActivity extends AppCompatActivity {
+
+    EditText commentText;
+    StringBuilder reply=new StringBuilder();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_comment);
+        Intent intent=getIntent();
+        if(intent!=null){
+            reply.append("回复@"+intent.getStringExtra("username")+ ":\n\n");
+        }
+        commentText=findViewById(R.id.comment_text);
+    }
+
+    public void back(View view){
+        finish();
+    }
+
+    public void add(View view){
+
     }
 }
