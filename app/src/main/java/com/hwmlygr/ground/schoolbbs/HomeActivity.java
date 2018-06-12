44d2e5906ac0df150337259a6ded30991d0ef89c
18300192781 <617649002@qq.com>
2018-06-12 11:49:18 +0800

    by:huang 主页面编写

--- app/src/main/java/com/hwmlygr/ground/schoolbbs/HomeActivity.java
+++ app/src/main/java/com/hwmlygr/ground/schoolbbs/HomeActivity.java
@@ -1,14 +1,114 @@
 package com.hwmlygr.ground.schoolbbs;
 
-import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class HomeActivity extends Activity {
+import com.viewpagerindicator.TabPageIndicator;
+
+import java.util.ArrayList;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private String[] mTitle;
+    private ArrayList<ListView> mTopicListView;
+    private Toolbar tb_home;
+    private ViewPager vp_home;
+    private TabPageIndicator tpi_indicator;
+    private DBHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initData();
+        initView();
+    }
 
+    private void initData() {
+        mDbHelper = new DBHelper(getApplicationContext());
+        mTitle = new String[]{"学习","吐槽","美食","游戏"};
+        mTopicListView = new ArrayList<>(4);
+        for (int i = 0;i < 4; i++){
+            ListView listView = new ListView(getApplicationContext());
+            listView.setAdapter(new TopicListAdapter(mTitle[i]));
+            mTopicListView.add(listView);
+        }
+    }
+
+    private void initView() {
+        tb_home = findViewById(R.id.tb_home);
+        vp_home = findViewById(R.id.vp_home);
+        tpi_indicator = findViewById(R.id.tpi_indicator);
+
+        vp_home.setAdapter(new IndicatorAdapter());
+        tpi_indicator.setViewPager(vp_home);
+    }
+    private class TopicListAdapter extends BaseAdapter{
+        String topic;
+        public TopicListAdapter(String topic) {
+            this.topic = topic;
+        }
+
+        @Override
+        public int getCount() {
+            return 1;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return topic;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView textView = new TextView(getApplicationContext());
+            textView.setText(topic);
+            textView.setTextColor(Color.BLACK);
+            return textView;
+        }
+    }
+    private class IndicatorAdapter extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return mTopicListView.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public ListView instantiateItem(ViewGroup container, int position) {
+            ListView listview = mTopicListView.get(position);
+            container.addView(listview);
+            return listview;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View)object);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitle[position];
+        }
     }
 }
--- app/src/main/java/com/hwmlygr/ground/schoolbbs/RegisterActivity.java
+++ app/src/main/java/com/hwmlygr/ground/schoolbbs/RegisterActivity.java
@@ -1,23 +1,36 @@
 package com.hwmlygr.ground.schoolbbs;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends Activity {
+public class RegisterActivity extends Activity implements View.OnClickListener {
 
     private EditText edt_user_name;
     private EditText edt_password;
     private EditText edt_repassword;
     private Button btn_register;
+    private DBHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initData();
         initView();
+
+    }
+
+    private void initData() {
+        mDbHelper = new DBHelper(getApplicationContext());
+        mDbHelper.open();
     }
 
     private void initView() {
@@ -25,5 +38,45 @@ public class RegisterActivity extends Activity {
         edt_password = findViewById(R.id.edt_password);
         edt_repassword = findViewById(R.id.edt_repassword);
         btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_register:
+                register();
+                break;
+        }
+    }
+
+    private void register() {
+        String userName = edt_user_name.getText().toString().trim();
+        String password = edt_password.getText().toString().trim();
+        String repassword = edt_repassword.getText().toString().trim();
+        if(TextUtils.isEmpty(userName)){
+            Toast.makeText(this, "用户名不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Cursor cursor = mDbHelper.query(DBHelper.USER_INFO, null, DBHelper.USER_NAME + "=?", new String[]{userName});
+//        判断存在
+        if(cursor != null && cursor.getCount() != 0 && cursor.moveToNext()){
+            String dbUserName = cursor.getString(cursor.getColumnIndex(DBHelper.USER_NAME));
+            if (dbUserName.equals(userName)) {
+                Toast.makeText(getApplicationContext(),"用户已存在",Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+//        注册用户
+        if(!password.equals(repassword)){
+            Toast.makeText(this, "密码不一致", Toast.LENGTH_SHORT).show();
+        }
+        ContentValues values = new ContentValues();
+        values.put(DBHelper.USER_NAME,userName);
+        values.put(DBHelper.PASSWORD,password);
+        if (mDbHelper.insert(DBHelper.USER_INFO, values)>0) {
+            Toast.makeText(this, "注册成功", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 }
